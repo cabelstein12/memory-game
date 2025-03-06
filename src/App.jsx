5b981796fabf27b88149a9bbcaa39c05d75cf24a
cabelstein12 <Caleb.Steinwandt@gmail.com>
2025-03-06 09:59:15 -0700

    template layout and basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Children, useState } from 'react'
 import './App.css'
+import './Cards.css'
+import './style.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  function Scoreboard(){
+    const [score, setScore] = useState(0);
+    const [highScore, setHighScore] = useState(0);
+    return (
+      <div id='scoreboard'>
+        <h4>Memory Game</h4>
+        <div id='current-score'>Current Score: {score}</div>
+        <div id='high-score'>High Score: {highScore}</div>
+      </div> 
+    )
+  }
+
+  function Cards(){
+    return(
+      <div id='card-group'>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+      </div>
+    )
+  }
+
+  function Card(){
+    return(
+    <div className='card'>
+      <img src='' alt=""  />
+    </div>
+    )
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Scoreboard />
+      <Cards />
     </>
   )
 }
