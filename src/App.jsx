be39033bd4b31dde2d6a6fe47a76a90fb31eb521
cabelstein12 <Caleb.Steinwandt@gmail.com>
2025-03-29 15:41:22 -0600

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,23 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import './Cards.css';
 import './style.css';
-import getPokemon from './Api';
+// import getPokemon from './Api';
+
+async function getPokemon(randomNumber){
+  async function assignPokemon(){
+      const pokemon = {}
+      await fetch("https://pokeapi.co/api/v2/pokemon/")
+          .then(r => r.json())
+          .then(result => pokemon.name = result.results[randomNumber].name);
+      await fetch("https://pokeapi.co/api/v2/pokemon/")
+          .then(r => r.json())
+          .then(result => result.results[randomNumber].url)
+          .then(url => fetch(url).then(r=> r.json()
+          .then(data => pokemon.image = data.sprites.front_default)))
+      return pokemon;
+  }
+  return assignPokemon();
+}
 
 function App() {
   const [score, setScore] = useState(0);
